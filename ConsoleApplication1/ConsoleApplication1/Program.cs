74a3048a88199597719a6948c67b6b9ea31b7fd6
Satoshi Terasaki <Satoshi Terasaki>
2016-06-22 22:10:38 +0900

    add ConsoleApplication to show number table which shows i^k for i in 10 and for k in 1,2,3,4,1/2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Title = "number table";
+            Console.WriteLine("     ^2      ^3      ^4      sqrt");
+            Console.WriteLine("---- -------- -------- --------");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(" {0,2}   {1,5}   {2,5}   {3,5}   {4:F4}", i, i * i, i * i * i,i*i*i*i, Math.Sqrt(i));
+            }
+            Console.Read();
+            return 0;
+        }
+    }
+}
