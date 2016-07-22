465ebe351cd122e70399891552eed597049cd13f
Satoshi Terasaki <Satoshi Terasaki>
2016-07-22 23:10:56 +0900

    add winform 3 and remove min max box

--- VS2013/SimpleCalcUsingChildForm/SimpleCalcUsingChildForm/Form3.Designer.cs
+++ VS2013/SimpleCalcUsingChildForm/SimpleCalcUsingChildForm/Form3.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SimpleCalcUsingChildForm
 {
-    partial class Form2
+    partial class Form3
     {
         /// <summary>
         /// Required designer variable.
@@ -28,34 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Label1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Label1
+            // label1
             // 
-            this.Label1.AutoSize = true;
-            this.Label1.Location = new System.Drawing.Point(53, 60);
-            this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(179, 18);
-            this.Label1.TabIndex = 2;
-            this.Label1.Text = "Aの値を設定してください";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bの値を入力してください";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 112);
+            this.label2.Location = new System.Drawing.Point(56, 83);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 18);
             this.label2.TabIndex = 1;
-            this.label2.Text = "A=";
+            this.label2.Text = "B=";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(91, 104);
+            this.textBox1.Location = new System.Drawing.Point(100, 75);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 25);
             this.textBox1.TabIndex = 0;
@@ -63,7 +63,7 @@
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(91, 197);
+            this.button1.Location = new System.Drawing.Point(100, 151);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -73,27 +73,27 @@
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button2.Location = new System.Drawing.Point(219, 196);
+            this.button2.Location = new System.Drawing.Point(216, 150);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(88, 24);
+            this.button2.Size = new System.Drawing.Size(84, 24);
             this.button2.TabIndex = 4;
             this.button2.Text = "取り消し";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // Form2
+            // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 360);
+            this.ClientSize = new System.Drawing.Size(547, 385);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "Form2";
-            this.Text = "Aの値を入力";
+            this.Name = "Form3";
+            this.Text = "Bの値を入力";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,7 +101,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
